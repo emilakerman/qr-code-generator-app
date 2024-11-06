f5be69944f345baa9e7ee47b2c51be89f250e155
emilakerman <emilaakerman@gmail.com>
2024-11-06 20:04:50 +0100

    added save qr code feature

--- app/src/main/java/com/emilakerman/qrcodegenereator/MainActivity.kt
+++ app/src/main/java/com/emilakerman/qrcodegenereator/MainActivity.kt
@@ -1,24 +1,39 @@
 package com.emilakerman.qrcodegenereator
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr.bitmap
+import android.content.ContentValues
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.drawToBitmap
 import com.emilakerman.qrcodegenereator.databinding.ActivityMainBinding
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.WriterException
 import com.journeyapps.barcodescanner.BarcodeEncoder
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        /*var bitMap = binding.qrCodeImage.drawToBitmap();*/
 
         binding.generateButton.setOnClickListener {
             generateQrCode()
         }
         binding.saveButton.setOnClickListener {
-            // saves it
+            saveMediaToStorage(binding.qrCodeImage.drawToBitmap())
         }
     }
     private fun generateQrCode() {
@@ -29,6 +44,42 @@ class MainActivity : AppCompatActivity() {
             binding.qrCodeImage.setImageBitmap(bitmap)
         } catch(e : WriterException) {
             println(e);
+        }
+    }
+    fun saveMediaToStorage(bitmap: Bitmap) {
+        val filename = "${System.currentTimeMillis()}.jpg"
+        var fos: OutputStream? = null
+        //For devices running android >= Q
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            //getting the contentResolver
+            contentResolver?.also { resolver ->
+
+                //Content resolver will process the contentvalues
+                val contentValues = ContentValues().apply {
+
+                    //putting file information in content values
+                    put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+                    put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
+                    put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                }
+                //Inserting the contentValues to contentResolver and getting the Uri
+                val imageUri: Uri? =
+                    resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+
+                //Opening an outputstream with the Uri that we got
+                fos = imageUri?.let { resolver.openOutputStream(it) }
+            }
+        } else {
+            //These for devices running on android < Q
+            //So I don't think an explanation is needed here
+            val imagesDir =
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+            val image = File(imagesDir, filename)
+            fos = FileOutputStream(image)
+        }
+        fos?.use {
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, it)
+            Toast.makeText(this, "QR Code Saved to Gallery!", Toast.LENGTH_SHORT).show()
         }
     }
 }
